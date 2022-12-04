97d276f75252ea0c1efd25fbad40eb186e1aac86
Michal Cieslar <michalovskyyy@gmail.com>
2022-12-04 20:23:27 +0100

    add authorization to getting messages

--- infrastructure/src/stacks/restApi/lambdas/getMessages/getMessagesLambdaHandler.ts
+++ infrastructure/src/stacks/restApi/lambdas/getMessages/getMessagesLambdaHandler.ts
@@ -1,14 +1,23 @@
 import 'reflect-metadata';
 
-import { APIGatewayEvent, APIGatewayProxyEventQueryStringParameters, Handler, ProxyResult } from 'aws-lambda';
+import { APIGatewayEvent, Handler, ProxyResult } from 'aws-lambda';
 
-import { QueryBuilderFactoryImpl, MessageMapperImpl, MessageRepositoryImpl, FindMessagesQueryImpl } from 'rest-api';
+import {
+  QueryBuilderFactoryImpl,
+  MessageMapperImpl,
+  MessageRepositoryImpl,
+  FindMessagesQueryImpl,
+  TokenServiceImpl,
+  VerifyAccessTokenQueryImpl,
+} from 'rest-api';
 import { EnvKey } from '../../../../config/envKey.js';
 
 const databaseName = process.env[EnvKey.databaseName] as string;
 const host = process.env[EnvKey.databaseHost] as string;
 const user = process.env[EnvKey.databaseUser] as string;
 const databasePassword = process.env[EnvKey.databasePassword] as string;
+const jwtSecret = process.env[EnvKey.jwtSecret] as string;
+const jwtExpiresIn = parseInt(process.env[EnvKey.jwtExpiresIn] as string);
 
 const databaseQueryBuilder = new QueryBuilderFactoryImpl().create({
   databaseName,
@@ -20,9 +29,15 @@ const databaseQueryBuilder = new QueryBuilderFactoryImpl().create({
 const messageMapper = new MessageMapperImpl();
 const messageRepositoryImpl = new MessageRepositoryImpl(databaseQueryBuilder, messageMapper);
 const findMessagesQueryImpl = new FindMessagesQueryImpl(messageRepositoryImpl);
+const tokenService = new TokenServiceImpl(jwtSecret, jwtExpiresIn);
+const verifyAccessTokenQuery = new VerifyAccessTokenQueryImpl(tokenService);
 
 export const lambda: Handler = async (event: APIGatewayEvent): Promise<ProxyResult> => {
-  const { userId } = event.queryStringParameters as APIGatewayProxyEventQueryStringParameters;
+  const authorizationHeader = event.headers['Authorization'];
+
+  const [_, accessToken] = authorizationHeader?.split(' ') as string[];
+
+  const { userId } = await verifyAccessTokenQuery.verifyAccessToken({ accessToken: accessToken as string });
 
   const { messages } = await findMessagesQueryImpl.findMessages({ userId: userId as string });
 
