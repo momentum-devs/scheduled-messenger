d653723be9550c57ab1b736fde64b9dbc27bdd90
Michal Cieslar <michalovskyyy@gmail.com>
2022-12-10 16:40:46 +0100

    fix cron expression

--- infrastructure/src/stacks/messagesProcessing/messagesProcessingStack.ts
+++ infrastructure/src/stacks/messagesProcessing/messagesProcessingStack.ts
@@ -42,7 +42,7 @@ export class MessagesProcessingStack extends core.Stack {
     });
 
     const eventRule = new events.Rule(this, 'scheduleRule', {
-      schedule: events.Schedule.cron({ minute: '*/2' }),
+      schedule: events.Schedule.cron({ minute: '0/2' }),
     });
 
     eventRule.addTarget(new targets.LambdaFunction(sendMessagesLambda));
